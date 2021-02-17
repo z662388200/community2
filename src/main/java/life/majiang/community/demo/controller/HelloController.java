48ce7f0e7008416e2fe38e51ea2778999dec87bf
Zhang Yi <914281275@qq.com>
2021-02-17 13:02:24 +0800

    第一次提交

---
+++ src/main/java/life/majiang/community/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package life.majiang.community.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller   //允许这个类去接收前端的一个请求
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name,Model model){
+        model.addAttribute("name",name);//给model赋值
+        return"hello";//然后我们在模板目录里面创建一个hello的html文件
+    }
+}
