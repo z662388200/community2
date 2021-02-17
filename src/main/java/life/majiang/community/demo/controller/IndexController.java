27e3341d35618931c11130e9a72d9dd338247111
Zhang Yi <914281275@qq.com>
2021-02-18 00:28:48 +0800

    增加了导航栏

--- src/main/java/life/majiang/community/demo/controller/IndexController.java
+++ src/main/java/life/majiang/community/demo/controller/IndexController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller   //允许这个类去接收前端的一个请求
-public class HelloController {
+public class IndexController {
     @GetMapping("/hello")
     public String hello(@RequestParam(name="name") String name,Model model){
         model.addAttribute("name",name);//给model赋值
-        return"hello";//然后我们在模板目录里面创建一个hello的html文件
+        return "index";//然后我们在模板目录里面创建一个hello的html文件
     }
 }
